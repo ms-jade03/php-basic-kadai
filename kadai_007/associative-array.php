595d0deb171662592150774a1310b25670ecfa06
ms-jade03 <ms_jade_03_0501@yahoo.co.jp>
2024-07-17 22:16:41 +0900

    kadai_007フォルダを追加
    kadai_007フォルダを追加しました。
    ご確認をよろしくお願いいたします。

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,16 @@
+<!DCTYPE html>
+<html lang="ja">
+  <head>
+    <mata charset="utf-8">
+    <title>PHP kadai_007</title>
+  </head>
+
+  <boby>
+    <p>
+      <?php
+       $commodity_data = ['name' => 'onion' , 'price' => 200 , 'weight' => 160];
+       print_r($commodity_data);
+      ?>
+    </p>
+  </body>
+</html>
